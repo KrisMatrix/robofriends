96c7305323b877de1d578bea83568b699337d132
Krishna Karnamadakala <krishna@hindulit.com>
2021-09-15 03:04:14 -0400

    Added more files and directories to git repo

---
+++ src/robots.js
@@ -0,0 +1,38 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Krishna K',
+    username: 'KK',
+    email: 'krishna@jan.biz'
+  },
+  {
+    id: 2,
+    name: 'Megan C',
+    username: 'MZC',
+    email: 'megan@feb.biz'
+  },
+  {
+    id: 3,
+    name: 'Vishaala W',
+    username: 'VW',
+    email: 'vishaala@mar.biz'
+  },
+  {
+    id: 4,
+    name: 'Jagannatha S',
+    username: 'JS',
+    email: 'jagannatha@apr.biz'
+  },
+  {
+    id: 5,
+    name: 'Subhadra S',
+    username: 'SS',
+    email: 'subhadra@may.biz'
+  },
+  {
+    id: 6,
+    name: 'Sohan J',
+    username: 'SJ',
+    email: 'sohan@jun.biz'
+  },
+]
